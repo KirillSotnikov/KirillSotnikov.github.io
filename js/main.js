570b4a40671335cdd280ed1af913087062bca03d
unknown <sotnikov_k@outlook.com>
2019-03-17 04:15:26 +0200

    product

--- js/main.js
+++ js/main.js
@@ -3,17 +3,41 @@ const pageId = $('main').attr('id');
 switch (pageId) {
     case 'partnersPage': {
         modalPartner();
+        menuFunc();
         break;
     }
     case 'productsPage':{
         menuFunc();
         break;
     }
+    case 'categoryPage':{
+        footerFunc();
+        menuFunc();
+    }
+    case 'singleProduct':{
+        footerFunc();
+        menuFunc();
+        productSlider();
+    }
     case 'productItem':{
         initSlider();
         modalPartner();
         break;
     }
+}
+
+function productSlider(){
+    $('.product_container_slider_big.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product_container_slider_small',
+    });
+    $('.product_container_slider_small.slider').slick({
+        slidesToShow: 3,
+        focusOnSelect: true,
+        asNavFor: '.product_container_slider_big',
+        slidesToScroll: 1,
+    });
 }
 
 function menuFunc(){
@@ -23,6 +47,21 @@ function menuFunc(){
     $('.menu_container_closer').on('click', function(){
         $('.menu_container').removeClass('active');
     });
+}
+
+function footerFunc(){
+    let winHeight = $('.footer').height();
+    let docHeight = $(window).height();
+
+    docHeight = docHeight - 120;
+    $('body').css('padding-bottom', winHeight);
+    console.log(winHeight);
+    console.log (docHeight);
+    if(docHeight < winHeight){
+        console.log(winHeight)
+        $('footer.footer').css('position', 'static');
+        $('body').css('padding-bottom', 0);
+    }
 }
 
 function modalPartner(){
